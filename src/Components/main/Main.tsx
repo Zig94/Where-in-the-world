9658c53731a60f31026047e5877787d9319f85f4
Maksymilian Szymanski <maksymilianszymanski2@gmail.com>
2023-09-11 18:42:48 +0200

    remove useRef from Main component

--- src/Components/main/Main.tsx
+++ src/Components/main/Main.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import CountryCard from '../countries/CountryCard'
 import CountyList from '../countries/CountyList'
 import SearchArea from '../searchArea/SearchArea'
